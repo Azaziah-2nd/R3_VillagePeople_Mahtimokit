5610930d4015c1d8d492477587140b5870ba070d
miika <miika.piitulainen@hotmail.com>
2017-04-21 12:43:57 +0300

    objektit
    kaikki objektien nimet oikaistu

--- R3_VillagePeople_Mahtimokit/AreYouSure_popup.designer.cs
+++ R3_VillagePeople_Mahtimokit/AreYouSure_popup.designer.cs
@@ -28,49 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_AreYouSure = new System.Windows.Forms.Label();
-            this.btn_Yes = new System.Windows.Forms.Button();
-            this.btn_No = new System.Windows.Forms.Button();
+            this.lbl_Are_You_Sure = new System.Windows.Forms.Label();
+            this.btn_Are_You_Sure_No = new System.Windows.Forms.Button();
+            this.btn_Are_You_Sure_Yes = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lbl_AreYouSure
+            // lbl_Are_You_Sure
             // 
-            this.lbl_AreYouSure.AutoSize = true;
-            this.lbl_AreYouSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_AreYouSure.Location = new System.Drawing.Point(113, 32);
-            this.lbl_AreYouSure.Name = "lbl_AreYouSure";
-            this.lbl_AreYouSure.Size = new System.Drawing.Size(111, 20);
-            this.lbl_AreYouSure.TabIndex = 0;
-            this.lbl_AreYouSure.Text = "Oletko varma?";
+            this.lbl_Are_You_Sure.AutoSize = true;
+            this.lbl_Are_You_Sure.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Are_You_Sure.Location = new System.Drawing.Point(113, 32);
+            this.lbl_Are_You_Sure.Name = "lbl_Are_You_Sure";
+            this.lbl_Are_You_Sure.Size = new System.Drawing.Size(111, 20);
+            this.lbl_Are_You_Sure.TabIndex = 0;
+            this.lbl_Are_You_Sure.Text = "Oletko varma?";
             // 
-            // btn_Yes
+            // btn_Are_You_Sure_No
             // 
-            this.btn_Yes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Yes.Location = new System.Drawing.Point(233, 69);
-            this.btn_Yes.Name = "btn_Yes";
-            this.btn_Yes.Size = new System.Drawing.Size(83, 39);
-            this.btn_Yes.TabIndex = 1;
-            this.btn_Yes.Text = "Ei";
-            this.btn_Yes.UseVisualStyleBackColor = true;
+            this.btn_Are_You_Sure_No.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Are_You_Sure_No.Location = new System.Drawing.Point(233, 69);
+            this.btn_Are_You_Sure_No.Name = "btn_Are_You_Sure_No";
+            this.btn_Are_You_Sure_No.Size = new System.Drawing.Size(83, 39);
+            this.btn_Are_You_Sure_No.TabIndex = 1;
+            this.btn_Are_You_Sure_No.Text = "Ei";
+            this.btn_Are_You_Sure_No.UseVisualStyleBackColor = true;
             // 
-            // btn_No
+            // btn_Are_You_Sure_Yes
             // 
-            this.btn_No.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_No.Location = new System.Drawing.Point(30, 69);
-            this.btn_No.Name = "btn_No";
-            this.btn_No.Size = new System.Drawing.Size(82, 39);
-            this.btn_No.TabIndex = 2;
-            this.btn_No.Text = "Kyllä";
-            this.btn_No.UseVisualStyleBackColor = true;
+            this.btn_Are_You_Sure_Yes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Are_You_Sure_Yes.Location = new System.Drawing.Point(30, 69);
+            this.btn_Are_You_Sure_Yes.Name = "btn_Are_You_Sure_Yes";
+            this.btn_Are_You_Sure_Yes.Size = new System.Drawing.Size(82, 39);
+            this.btn_Are_You_Sure_Yes.TabIndex = 2;
+            this.btn_Are_You_Sure_Yes.Text = "Kyllä";
+            this.btn_Are_You_Sure_Yes.UseVisualStyleBackColor = true;
             // 
             // AreYouSure_popup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(352, 123);
-            this.Controls.Add(this.btn_No);
-            this.Controls.Add(this.btn_Yes);
-            this.Controls.Add(this.lbl_AreYouSure);
+            this.Controls.Add(this.btn_Are_You_Sure_Yes);
+            this.Controls.Add(this.btn_Are_You_Sure_No);
+            this.Controls.Add(this.lbl_Are_You_Sure);
             this.Name = "AreYouSure_popup";
             this.Text = "Varmistus";
             this.ResumeLayout(false);
@@ -80,8 +80,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lbl_AreYouSure;
-        private System.Windows.Forms.Button btn_Yes;
-        private System.Windows.Forms.Button btn_No;
+        private System.Windows.Forms.Label lbl_Are_You_Sure;
+        private System.Windows.Forms.Button btn_Are_You_Sure_No;
+        private System.Windows.Forms.Button btn_Are_You_Sure_Yes;
     }
 }
