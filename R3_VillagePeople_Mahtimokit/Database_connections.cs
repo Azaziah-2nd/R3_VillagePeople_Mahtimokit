e90dd5b373f9a2fa86d792464f9d553b93590f64
Olli Suutari <suutari.olli@gmail.com>
2017-05-09 23:48:03 +0300

    Lisättiin kaksi uutta luokkaa : Common_methods ja Database_connections
    Lisättiin kaksi uutta luokkaa : Common_methods ja Database_connections

---
+++ R3_VillagePeople_Mahtimokit/Database_connections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3_VillagePeople_Mahtimokit
+{
+    public partial class Database_connections : Common_methods
+    {
+
+    }
+}
